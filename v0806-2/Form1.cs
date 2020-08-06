28fdb9c52a19ec2f065724142cc65bab40c6fdf2
fusatoshi3104 <satoshidat@gmail.com>
2020-08-06 11:51:40 +0900

    10％スピード

--- v0806-2/Form1.cs
+++ v0806-2/Form1.cs
@@ -12,9 +12,33 @@ namespace v0806_2
 {
     public partial class Form1 : Form
     {
+        int vx = -20;
+        int vy = -20;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+             label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left<0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label1.Left + label1) ;
         }
     }
 }
